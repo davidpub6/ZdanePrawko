bd75b8e318cee6b7dc289b561d42748a986931a2
KamilPolitSzkola <148445211+KamilPolitSzkola@users.noreply.github.com>
2025-05-23 09:25:23 +0200

    dzialy

--- app/src/App.js
+++ app/src/App.js
@@ -8,6 +8,10 @@ import Jazdy from './pages/Jazdy';
 import Egzamin from './pages/Egzamin';
 import Nauka from './pages/Nauka';
 
+import Tile1Page from './pages/nauka/dzial1/Tile1'; // Import Tile 1 page
+import Tile2Page from './pages/nauka/dzial1/Tile2'; // Import Tile 2 page
+import Tile3Page from './pages/nauka/dzial1/Tile3'; // Import Tile 3 page
+
 function App() {
     return (
         <div>
@@ -19,6 +23,11 @@ function App() {
                     <Route path="/jazdy" element={<Jazdy />} />
                     <Route path="/egzamin" element={<Egzamin />} />
                     <Route path="/nauka" element={<Nauka />} />
+
+                    {/* Add routes for Nauka tiles */}
+                    <Route path="/nauka/dzial1/tile1" element={<Tile1Page />} />
+                    <Route path="/nauka/dzial1/tile2" element={<Tile2Page />} />
+                    <Route path="/nauka/dzial1/tile3" element={<Tile3Page />} />
                 </Route>
                 </Routes>
             </Router>
