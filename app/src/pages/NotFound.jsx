13229ee4c7bf72bb6df42136553407ab0f139284
davidpub6 <davidpub6@gmail.com>
2025-05-31 21:04:13 +0200

    made admin page

--- app/src/pages/NotFound.jsx
+++ app/src/pages/NotFound.jsx
@@ -5,7 +5,7 @@ function NotFound() {
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
             <h1 className="text-4xl font-bold text-red-500 mb-4">404</h1>
             <p className="text-lg text-gray-700">Strona, której szukasz, nie istnieje.</p>
-            <a href="/" className="mt-4 text-blue-500 hover:underline">
+            <a href="/jazdy" className="mt-4 text-blue-500 hover:underline">
                 Wróć na stronę główną
             </a>
         </div>
